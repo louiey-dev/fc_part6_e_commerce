f7d77164ad2e8fc53102fd50f6df29a53fb9400c
Louie Yang <louiey.dev@gmail.com>
2024-02-26 21:18:05 +0900

    Ch04-06 Cubit을 이용한 appBar 개발

--- lib/presentation/main/component/top_app_bar/home_app_bar.dart
+++ lib/presentation/main/component/top_app_bar/home_app_bar.dart
@@ -1,52 +1,87 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
 
 import '../../../../core/theme/constant/app_icons.dart';
+import '../../../../core/theme/custom/custom_theme.dart';
+import '../../cubit/mall_type_cubit.dart';
 
 class HomeAppBar extends StatelessWidget {
   const HomeAppBar({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Theme.of(context).colorScheme.primary,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 6, horizontal: 8),
-        child: AppBar(
-          backgroundColor: Colors.transparent,
-          centerTitle: true,
-          title: Text(
-            "tabBar",
-            style: TextStyle(
-                fontWeight: FontWeight.bold, fontSize: 20, color: Colors.white),
-          ),
-          leading: Padding(
-            padding: EdgeInsets.all(8.0),
-            child: SvgPicture.asset(AppIcons.mainLogo),
-          ),
-          leadingWidth: 86,
-          actions: [
-            Padding(
-              padding: const EdgeInsets.all(4.0),
-              child: SvgPicture.asset(
-                AppIcons.location,
-                colorFilter: ColorFilter.mode(
-                    Theme.of(context).colorScheme.background, BlendMode.srcIn),
-                // color: Theme.of(context).colorScheme.background,
+    return BlocBuilder<MallTypeCubit, MallType>(
+      builder: (_, state) {
+        return AnimatedContainer(
+          duration: Duration(milliseconds: 400),
+          color: (state.isMarket)
+              ? Theme.of(context).colorScheme.primary
+              : Theme.of(context).colorScheme.background,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 6, horizontal: 8),
+            child: AppBar(
+              leading: Padding(
+                padding: EdgeInsets.all(8.0),
+                child: SvgPicture.asset(
+                  AppIcons.mainLogo,
+                  colorFilter: ColorFilter.mode(
+                      state.isMarket
+                          ? Theme.of(context).colorScheme.onPrimary
+                          : Theme.of(context).colorScheme.primary,
+                      BlendMode.srcIn),
+                ),
               ),
+              backgroundColor: Colors.transparent,
+              centerTitle: true,
+              title: DefaultTabController(
+                  initialIndex: state.index,
+                  length: MallType.values.length,
+                  child: TabBar(
+                    onTap: (index) =>
+                        context.read<MallTypeCubit>().changeIndex(index),
+                    labelColor: Colors.black,
+                    unselectedLabelColor: Colors.black,
+                    tabs: List.generate(
+                      MallType.values.length,
+                      (index) => Tab(
+                        text: MallType.values[index].toName,
+                      ),
+                    ),
+                  )),
+              leadingWidth: 86,
+              actions: [
+                Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: SvgPicture.asset(
+                    AppIcons.location,
+                    colorFilter: ColorFilter.mode(
+                        state.isMarket
+                            ? Theme.of(context).colorScheme.background
+                            : Theme.of(context).colorScheme.contentPrimary,
+                        BlendMode.srcIn),
+                    // color: Theme.of(context).colorScheme.background,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: SvgPicture.asset(
+                    AppIcons.cart,
+                    colorFilter: ColorFilter.mode(
+                        state.isMarket
+                            ? Theme.of(context).colorScheme.background
+                            : Theme.of(context).colorScheme.contentPrimary,
+                        BlendMode.srcIn),
+                    // color: Theme.of(context).colorScheme.background,
+                  ),
+                )
+              ],
             ),
-            Padding(
-              padding: const EdgeInsets.all(4.0),
-              child: SvgPicture.asset(
-                AppIcons.cart,
-                colorFilter: ColorFilter.mode(
-                    Theme.of(context).colorScheme.background, BlendMode.srcIn),
-                // color: Theme.of(context).colorScheme.background,
-              ),
-            )
-          ],
-        ),
-      ),
+          ),
+        );
+      },
     );
   }
 }
